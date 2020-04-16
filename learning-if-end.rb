05b5a9c64dec0ae141111fd133194a3aceafe784
Elvis Ning <ningelvis@gmail.com>
2020-04-16 19:07:12 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,37 @@
+#run_code_inside = FALSE
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+#chance_of_rain = 1 
+#puts "Let's go outside!"
+#if chance_of_rain > 0.5 
+# puts "Pack an umbrella!"
+#else 
+#  puts "Enjoy the fine day!"
+#end 
+#puts "Oh, and always wear suncscreen!"
+
+#chance_of_rain = 0.9
+#if chance_of_rain <= 0.25 
+#  puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella"
+#else 
+#  puts "Stay home and read Hegel."
+#end
+
+#0.0000001 Pack a sun shelter!
+#0.2 Pack a sun shelter!
+#0.200001 Pack a sun shelter!
+#0.3 Pack an umbrella!
+#0.9 Stay home and read Hegel.
+#1000 Stay home and read Hegel.
+#-23 Pack a sun shelter!
+
+puts "You know what year it is??"
+this_year = 2020
+puts "Hey, it's 2020!" if this_year == 2020
+
